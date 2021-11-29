1e5395956a381537667378c14bbc8275dab55977
rafaelccorrea <rafaelgustavocorrea@gmail.com>
2021-11-29 20:05:07 -0300

    GraphQL

---
+++ api/index.js
@@ -0,0 +1,33 @@
+const { ApolloServer } = require('apollo-server')
+const { mergeTypeDefs } = require('graphql-tools')
+const path = require('path')
+
+const { userSchema, userResolvers, UsersAPI } = require('./user')
+const { turmaSchema, turmaResolvers, TurmasAPI } = require('./turma')
+const { matriculaSchema } = require('./matricula')
+
+const typeDefs = mergeTypeDefs([userSchema, turmaSchema, matriculaSchema])
+const resolvers = [userResolvers, turmaResolvers]
+
+const dbConfig = {
+  client: 'sqlite3',
+  useNullAsDefault: true,
+  connection: {
+    filename: path.resolve(__dirname, './data/database.db')
+  }
+}
+
+const server = new ApolloServer( { 
+  typeDefs,
+  resolvers,
+  dataSources: () => {
+    return {
+      usersAPI: new UsersAPI(),
+      turmasAPI: new TurmasAPI(dbConfig)
+    }
+  },
+ })
+
+server.listen().then(({url}) => {
+  console.log(`Servidor rodando na porta ${url}`)
+})
